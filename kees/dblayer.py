c354562f5803437979a8201cedfcf20855f7b12d
Chozabu <chozabu@gmail.com>
2015-04-09 17:13:31 +0100

    switch backend to sqlalchemy

--- kees/dblayer.py
+++ kees/dblayer.py
@@ -1,189 +1,73 @@
+__author__ = 'chozabu'
+import dataset
+from OrmDefs import *
+fp = 'mydatabase.sqlite'
+# Remove the existing orm_in_detail.sqlite file
+if os.path.exists(fp):
+    os.remove(fp)
+
+from sqlalchemy import create_engine
+engine = create_engine('sqlite:///mydatabase.sqlite')
+#db = dataset.connect('sqlite:///mydatabase.db')
+
+from sqlalchemy.orm import sessionmaker
+session = sessionmaker()
+session.configure(bind=engine)
+Base.metadata.create_all(engine)
+
+s = session()
+#db
+
 import datetime, time
 import dataset
 import uuid, hashlib
 
 #db = dataset.connect('sqlite:///:memory:')
 sessions = {}
-db = dataset.connect('sqlite:///mydatabase.db')
-'''database=db
+#db = dataset.connect('sqlite:///mydatabase.db')
 
-table = db['sometable']
-table.insert(dict(name='John Doe', age=37))
-table.insert(dict(name='Jane Doe', age=34, gender='female'))
+#v = VotePost(author=chozabu, post=testpoint,value=.5)
 
-john = table.find_one(name='John Doe')
+def listProposals():
+	#s=session()
+	sqp = s.query(Post).all()
+	return [x.__dict__ for x in sqp]
 
-print john
-'''
+def viewProposal(prop_id):
+	return [x.__dict__ for x in s.query(Post).filter(Post.id==prop_id).all()]
 
-def add_vote(session, val, docid, dbid):
-	#security check
-	sRef = getsession(session)
-	if not sRef:
-		return False
-	print val, docid, dbid
-	levelstable = db[dbid]
-	for l in levelstable:
-		print l
-	record = levelstable.find_one(uid=docid)
-	print docid
-	print record, record==None
-	if record == None:
-		return {'result':'fail', 'reason':'voteable item not found'}
-	username = sRef['username']
-
-	#get tables
-	votes_stable = db['votes']
-	votes_group_table = db['vote_groups']
-	vote_group = votes_group_table.find_one(dbid=dbid, docid=docid)
-	if not vote_group:
-		vote_group = new_vote_group(docid=docid, dbid=dbid)
-	votenum=vote_group['ratingCount']
-	rating=vote_group['rating']
-	print "Rating was, ", rating
-	print username,docid
-	vote = votes_stable.find_one(user=username, docid=docid)
-	print "-------------"
-	print "vote=",vote
-	print "val=", val
-	print ""
-	#count vote
-	if not vote:
-		vp1=float(votenum+1.)
-		print "vp1=",vp1
-		rating=rating/vp1*votenum+val/(vp1)
-		votenum=vp1
-		print "new vote, rating now, ", rating
-	else:
-		oldval=vote['value']
-		print oldval, votenum
-		if votenum<1:
-			rating=val
-		else:
-			rating=rating-oldval/votenum+val/votenum
-		print "re-vote, rating now, ", rating
-
-	vote_group['ratingCount']=votenum
-	vote_group['rating']=rating
-	votes_group_table.upsert(vote_group, ['uid'])
-	for v in votes_group_table:
-		print v
-
-	newVote = {"user":username, "value":val, "time":time.time(), "docid":docid}
-	votes_stable.upsert(newVote,['user', 'docid'])
-	print "xoxoxoxoxoxoxoxoxoxoxox"
-	for v in votes_stable:
-		print v
-	return True
-'''
-def get_tags(docid):
-	votes_group_table = db['vote_groups']
-	for v in votes_group_table:
-		print v['docid']
-	rawtags = votes_group_table.find(docid=docid)
-	tags = [x for x in rawtags]
-	print "tags=", tags
-	return tags
-'''
-def new_item(name, author):
-	newLevel = {}
-	newLevel['uid'] = uuid.uuid4().get_hex()
-	newLevel['name'] = name
-	newLevel['author'] = author
-	nowStamp = time.time()
-	newLevel['dateAdded'] = nowStamp
-	newLevel['dateModified'] = nowStamp
-	return newLevel
-
-def new_connection(a, b, author,ctype="depends"):
-	newLevel = {}
-	newLevel['uid'] = uuid.uuid4().get_hex()
-	newLevel['a'] = a
-	newLevel['b'] = b
-	newLevel['type'] = ctype
-	newLevel['author'] = author
-	nowStamp = time.time()
-	newLevel['dateAdded'] = nowStamp
-	newLevel['dateModified'] = nowStamp
-	return newLevel
-
-def new_vote_group(docid, dbid, tag):
-	newLevel = {}
-	newLevel['uid'] = uuid.uuid4().get_hex()
-	newLevel['docid'] = docid
-	newLevel['dbid'] = dbid
-	nowStamp = time.time()
-	newLevel['rating'] = .5
-	newLevel['ratingCount'] = 0
-	newLevel['dateAdded'] = nowStamp
-	newLevel['dateModified'] = nowStamp
-	return newLevel
-
-
-
-def add_connection(session, a, b, ctype):
-	ses = getsession(session)
+def createProposal(sid, PropText):
+	#s=session()
+	ses = getsession(sid)
 	if not ses:
 		return False
-	author = ses['username']
-	isNew = True
-	ctable = db['connections']
-	#TODO CHECK IF A AND B EXIST?
-	#record = levelstable.find_one(filename=fullname)
-	record = new_connection(a, b, author, ctype)
-	#db.ppLevels[fullname] = newLevel
-	ctable.insert(record)
-	return record
-
-def add_point(session, name, text):
-	ses = getsession(session)
+	author = authorFromSes(ses)
+	print author
+	#author = ses['username']
+	testpoint = Post(author=author, name=PropText)
+	s.add(testpoint)
+	s.commit()
+	
+def voteProposal(sid, prop_id, value):
+	#s=session()
+	ses = getsession(sid)
 	if not ses:
 		return False
-	author = ses['username']
-	isNew = True
-	levelstable = db['points']
-	#record = levelstable.find_one(filename=fullname)
-	record = new_item(name,author)
-	#record['filename'] = fullname
-	#record['downloads'] = 0
-	record['text'] = text
-	#db.ppLevels[fullname] = newLevel
-	levelstable.insert(record)
-	return record
-
-#find levels with information from other tables
-def complex_query_levels(sortKeydb='vote_groups', sortkeydata='rating', tag='boardz', tagmin=0.0, cursor=0, limit=8):
-	qs2='''
-SELECT pplevels.name, pplevels.uid, vote_groups.rating, vote_groups.tag
-FROM pplevels JOIN vote_groups
-ON pplevels.uid=vote_groups.docid
-WHERE vote_groups.tag="{}"
-AND vote_groups.rating>{}
-ORDER BY {}.{}
-limit {},{}
-'''.format(tag, tagmin, sortKeydb, sortkeydata, cursor, limit)
-	print qs2
-	result = db.query(qs2)
-
-	rlist = [r for r in result]
-	print "complex query", tag, tagmin, sortKeydb, sortkeydata, cursor, limit
-	print "returned", rlist
-	return rlist
-
-def query_points(sortKey, cursor=0, limit=8, **_filter):
-	levelstable = db['points']
-	print limit, cursor, sortKey
-	a= levelstable.find(_limit=limit, _offset=cursor, order_by=sortKey, **_filter)
-	return [x for x in a]
-def get_one(dbid, **_filter):
-	print "get_one"
-	print dbid
-	levelstable = db[dbid]
-	print levelstable
-	a= levelstable.find_one(**_filter)
-	print a
-	return a
-
+	author = authorFromSes(ses)
+	point = s.query(Post).get(prop_id)
+	v = VotePost(author=author, post=point,value=value)
+	s.add(v)
+	s.commit()
+def joinProposal(sid, prop_id, prop_id2, cType):
+	ses = getsession(sid)
+	if not ses:
+		return False
+	author = authorFromSes(ses)
+	point = s.query(Post).get(prop_id)
+	point2 = s.query(Post).get(prop_id2)
+	c = PostPostLink(post_from=point,post_to=point2,type=cType)
+	s.add(c)
+	s.commit()
 def getsession(session):
 	if sessions.has_key(session) == False:
 		return False
@@ -191,7 +75,14 @@ def getsession(session):
 	sRef['lastTouch'] = time.time()
 	return sRef
 
-def newSession(username, userRef):
+def authorFromSes(sref):
+	#s=session()
+	result = s.query(Author).filter(Author.name == sref['username']).all()
+	if len(result):
+		return result[0]
+	return None
+
+def newSession(userRef):
 	timeNow = time.time()
 	session = {}
 	session['_id'] = uuid.uuid4().get_hex()
@@ -199,8 +90,8 @@ def newSession(username, userRef):
 	session['lastTouch'] = timeNow
 	session['info'] = {}
 	sessions[session['_id']] = session
-	session['username'] = username
-	# TODO CHECK FOR TIMED OUT SESSIONS elsewhere
+	session['username'] = userRef.name
+	session['id'] = userRef.id
 	checkTimedOutSessions()
 	return session
 
@@ -216,16 +107,21 @@ def checkTimedOutSessions():
 		sessions.pop(s)
 
 def login(username, password):
+	#s=session()
 	username = username.replace(".","")
-	usertable = db['users']
-	eUser = usertable.find_one(username=username)
-	if(eUser):
+	#usertable = db['users']
+	#eUser = usertable.find_one(username=username)
+	sq = s.query(Author).filter(Author.name==username).all()
+	#print "---", sq
+	if len(sq):
+		eUser = sq[0]
+		#print eUser
 		# makeImage(username)
-		if hashlib.sha512(password + eUser['salt']).hexdigest() == eUser['password']:
-			session	 = newSession(username, eUser)
-			eUser['lastTouch'] = time.time()
-			usertable.update(eUser,['username'])
-			returnInfo = {"result":"OK", "session":session['_id'], "message":"logged in", "username":username, "_id":username, "noteCount":eUser['noteCount']}
+		if hashlib.sha512(password + eUser.salt).hexdigest() == eUser.password:
+			msession	 = newSession(eUser)
+			#eUser.lastTouch = time.time()
+			#usertable.update(eUser,['username'])
+			returnInfo = {"result":"OK", "session":msession['_id'], "message":"logged in", "username":username, "_id":username}#, "noteCount":eUser['noteCount']}
 			print returnInfo
 			return returnInfo
 		wrongpass = {"result":"fail", "message":"incorrect password"}
@@ -236,25 +132,16 @@ def login(username, password):
 	return wrongUser
 
 def new_user(username, password):
+	#s=session()
 	username = username.replace(".","")
-	usertable = db['users']
-	#eUser = database.users.find_one({"_id":username})
-	eUser = usertable.find_one(username=username)
-	print eUser
-	if(eUser):
+	
+	sq = s.query(Author).filter(Author.name==username).all()
+	if len(sq):
 		print "dupe account creation attempt on name: " + username
-		return login(username, password)
+		#return login(username, password)
 		return {"result":"fail", "message":"name taken"}
-	newUser = {}
-	newUser['createdAt'] = time.time()
-	newUser['lastTouch'] = time.time()
-	salt = newUser['salt'] = uuid.uuid4().get_hex()
-	#newUser['_id'] = username
-	#makeImage(username)
-	newUser['username'] = username
-	newUser['noteCount'] = 0
-	#newUser['groups'] = []
-	newUser['password'] = hashlib.sha512(password + salt).hexdigest()
-	usertable.insert(newUser)
-	#database.users.insert(newUser)
+	salt = uuid.uuid4().get_hex()
+	newuser = Author(name=username, password = hashlib.sha512(password + salt).hexdigest(), salt=salt)
+	s.add(newuser)
+	s.commit()
 	print {"result":"OK", "message":"account " + username + " created"}
--- kees/views.py
+++ kees/views.py
@@ -7,11 +7,6 @@ import dblayer
 import datetime
 from pyramid.response import Response
 
-
-if not os.path.exists("kees/pplevels"):os.makedirs("kees/pplevels")
-if not os.path.exists("kees/thumbs"):os.makedirs("kees/thumbs")
-if not os.path.exists("kees/crashs"):os.makedirs("kees/crashs")
-
 def pythonicVarName(field):
 	firstLetter = True
 	for word in field.split(' '):
@@ -85,18 +80,8 @@ def login(request):
 	password =  i['password']
 	return dblayer.login(username,password)
 
-@view_config(route_name='complex_query_levels', renderer='json')
-def complex_query_levels(request):
-	i = request.POST
-	'''sortKey =  str(i['sortKey'])
-	cursor =  int(i['cursor'])
-	limit =  int(i['limit'])'''
-	levels = dblayer.complex_query_levels()
-	#return Response("OK")
-	return levels
 
-@view_config(route_name='query_levels', renderer='json')
-def query_levels(request):
+
 	i = request.POST
 	sortKey =  str(i['sortKey'])
 	cursor =  int(i['cursor'])
@@ -105,19 +90,40 @@ def query_levels(request):
 	#return Response("OK")
 	return levels
 
-@view_config(route_name='query_points', renderer='json')
+@view_config(route_name='get_points', renderer='json')
+def get_points(request):
+	print "points"
+	#i = request.POST
+	#sortKey =  str(i['sortKey'])
+	#cursor =  int(i['cursor'])
+	#limit =  int(i['limit'])
+	#print "querying", sortKey,cursor,limit
+	points =dblayer.listProposals()
+	print "returning, ", points
+	return points
+@view_config(route_name='join_points', renderer='json')
+def join_points(request):
+	print "join"
+	#i = request.POST
+	cType =  str(i['type'])
+	a =  int(i['a'])
+	b =  int(i['b'])
+	session =  i['session']
+	#print "querying", sortKey,cursor,limit
+	return dblayer.joinProposal(session, a,b,cType)
+
+'''@view_config(route_name='query_points', renderer='json')
 def query_points(request):
 	print "points"
 	i = request.POST
 	sortKey =  str(i['sortKey'])
 	cursor =  int(i['cursor'])
 	limit =  int(i['limit'])
+	print "querying", sortKey,cursor,limit
 	points = dblayer.query_points(sortKey,cursor,limit)
-	print points
-	return points
+	print "returning, ", points
+	return points'''
 
-@view_config(route_name='get_tags', renderer='json')
-def get_tags(request):
 	print "TAGS"
 	i = request.POST
 	print i
@@ -127,7 +133,7 @@ def get_tags(request):
 	print tags
 	return tags
 
-@view_config(route_name='get_point', renderer='json')
+'''@view_config(route_name='get_point', renderer='json')
 def get_point(request):
 	print "HELLO"
 	i = request.POST
@@ -137,10 +143,8 @@ def get_point(request):
 	print "docid=",docid
 	point = dblayer.get_one(dbid, uid=docid)
 	print point
-	return point
+	return point'''
 
-@view_config(route_name='get_level', renderer='json')
-def get_level(request):
 	print "HELLO"
 	i = request.POST
 	print i
@@ -155,122 +159,18 @@ def get_level(request):
 def add_vote(request):
 	i = request.POST
 	print i
-	dbid =  str(i['dbid'])
-	print dbid
-	if dbid not in ['points', 'pplevels']:
-		print "db not accepted"
-		return {"status":"fail"}
-	print "db accepted"
 	session =  str(i['session'])
 	print "session=", session
 	val =  float(i['val'])
 	print "val=", val
 	docid =  str(i['docid'])
 	print "docid=", docid
-	tag = i.get('tag', None)
-
-	return dblayer.add_vote(session,val,docid,dbid,tag=tag)
+	return dblayer.voteProposal(session, docid, .9)
 
 @view_config(route_name='add_point', renderer='json')
 def add_point(request):
 	i = request.POST
 	session =  i['session']
 	print session
-	name =  i['name']
 	text =  i['text']
-	return dblayer.add_point(session, name, text)
-
-@view_config(route_name='uploadLevel', renderer='json')
-def uploadLevel(request):
-	i = request.POST
-	session =  i['session'].file.read()
-	print session
-	author =  i['author'].file.read()
-	name =  i['name'].file.read()
-	leveldata = i['leveldata'].file
-	sshot = i['sshot'].file
-	fullname = pythonicVarName(author+name)
-	tags = i.get("tags_str").file.read()
-	if dblayer.add_level(session, name, author, fullname, tags):
-
-
-		namepath = "kees/pplevels/" + fullname
-		write_a_file(namepath, leveldata)
-
-		namepath = "kees/thumbs/" + fullname+".png"
-		write_a_file(namepath, sshot)
-
-		return {"result":"OK"}
-	return {"result":"FAIL","dta":[session,author,name], "insessions":session in dblayer.sessions}
-	#return Response("OK")
-
-
-
-@view_config(route_name='uploadCrash', renderer='json')
-def uploadCrash(request):
-	i = request.POST
-	print "POST uploading crash"
-
-	now = str(datetime.datetime.now())
-	crashData = i['crashData']
-
-	namepath = "kees/crashs/" + now+"--" + str(i['version'])
-	write_some_data(namepath, crashData)
-
-	return {"result":"OK"}
-
-@view_config(route_name='crashLogs', renderer='json')
-def crashLogs(request):
-		files = os.listdir('kees/crashs')
-		#result = "".join('<a href="crashs/'+f+'" </>'+f+'</a><br/>' for f in files)
-		result = "".join('<a href="kees/crashs/'+f+'" </>'+f+'</a><br/>' for f in files)
-		return Response(body=result,headerlist=[('Content-Type', 'text/html')])
-
-
-@view_config(route_name='store_mp3_view', renderer='json')
-def store_mp3_view(request):
-	# ``filename`` contains the name of the file in string format.
-	#
-	# WARNING: this example does not deal with the fact that IE sends an
-	# absolute file *path* as the filename.  This example is naive; it
-	# trusts user input.
-
-	filename = request.POST['mp3'].filename
-
-	# ``input_file`` contains the actual file data which needs to be
-	# stored somewhere.
-
-	input_file = request.POST['mp3'].file
-
-	# Note that we are generating our own filename instead of trusting
-	# the incoming filename since that might result in insecure paths.
-	# Please note that in a real application you would not use /tmp,
-	# and if you write to an untrusted location you will need to do
-	# some extra work to prevent symlink attacks.
-
-	file_path = os.path.join('/tmp', '%s.mp3' % uuid.uuid4())
-
-	# We first write to a temporary file to prevent incomplete files from
-	# being used.
-
-	temp_file_path = file_path + '~'
-	output_file = open(temp_file_path, 'wb')
-
-	# Finally write the data to a temporary file
-	input_file.seek(0)
-	while True:
-		data = input_file.read(2<<16)
-		if not data:
-			break
-		output_file.write(data)
-
-	# If your data is really critical you may want to force it to disk first
-	# using output_file.flush(); os.fsync(output_file.fileno())
-
-	output_file.close()
-
-	# Now that we know the file has been fully saved to disk move it into place.
-
-	os.rename(temp_file_path, file_path)
-
-	return Response('OK')
+	return dblayer.createProposal(session, text)
